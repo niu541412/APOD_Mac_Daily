e0513dc2a173eed51a035256ec689d00780efd8c
Jomic <niu541412@gmail.com>
2019-01-15 22:34:10 +0800

    set back to random wallpapers provided by system.

--- random.sh
+++ random.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 /usr/bin/osascript <<END
 tell application "System Events"
- tell current desktop
- set picture rotation to 1 -- 0=off | 1=interval | 2=login | 3=sleep
- set random order to true
- set pictures folder to "/Library/Desktop Pictures"
- set change interval to 3600 -- seconds
- end tell
+	tell every desktop
+		set picture rotation to 1 -- 0=off | 1=interval | 2=login | 3=sleep
+		set random order to true
+		set pictures folder to "/Library/Desktop Pictures"
+		set change interval to 3600 -- seconds
+	end tell
 end tell
 END
